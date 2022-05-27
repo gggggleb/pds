2535d009073728300a3d3782ec47eb1ba7f19891
gleb <admin@glebmail.xyz>
2022-05-27 20:28:41 +0300

    edit path

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pds',
-    version='1.0',
+    version='1.1',
     description='Pds server',
     url='https://git.glebmail.xyz/PythonPrograms/pcsd',
     author='gleb',
